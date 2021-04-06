c3972f7843d3c2c8ff20079ff114e4c882ee3c25
jquinlan28 <70920081+jquinlan28@users.noreply.github.com>
2021-04-06 00:09:16 -0400

    Add files via upload
    Added button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,27 @@
-import React from "react";
-import { Card } from "react-bootstrap";
+import React from 'react';
+import styled from 'styled-components';
 
-export default function Button({
-  title,
-  text,
-  backgroundColor = "",
-  color = "",
-}) {
+const Butt = styled.button`
+
+  background-color: #F1F1F1;
+  border: none;
+  color: #1B7B89;
+  padding: 33px 30px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  font-family: "Open Sans", sans-serif;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 20px;
+  text-transform: uppercase
+  `;
+
+const Button = text => {
   return (
-    <Card style={{ backgroundColor: backgroundColor, color: color }}>
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{text}</Card.Text>
-      </Card.Body>
-    </Card>
-  );
+    <Butt> text </Butt>
+  )
 }
+
+export default Button;
