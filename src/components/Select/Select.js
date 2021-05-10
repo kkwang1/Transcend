99a807967e78e5d866424c866f1123740c00d3b9
cyl9011 <yoleichen@MacBook-Air.local>
2021-05-10 23:13:35 +0800

    add dropdown menus/select buttons component

---
+++ src/components/Select/Select.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { DropdownButton, Dropdown } from "react-bootstrap";
+import "./style.css";
+
+/* 
+Usage: create dropdown
+Description: 
+    - title: string
+    - options: string array
+*/
+export default function Select({ title, options }) {
+  console.log(title, options);
+  return (
+    <DropdownButton
+      id={`dropdown-variants-${title}`}
+      title={title}
+      style={{ display: "table-cell", padding: "5px" }}
+      className="dropdown-link"
+    >
+      {options.map((s) => (
+        <Dropdown.Item eventKey={s}>{s}</Dropdown.Item>
+      ))}
+    </DropdownButton>
+  );
+}
---
+++ src/components/Select/SelectGroup.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Select from "./Select";
+
+/* 
+Name: info
+Type: Array
+Description:
+  an array of objects that has the following properties (all required):
+    - title: string
+    - options: string array
+*/
+export default function SelectGroups({ info }) {
+  return (
+    <div style={{ display: "table", width: "100%" }}>
+      {info.map((s) => (
+        <Select title={s.title} options={s.options} />
+      ))}
+    </div>
+  );
+}
