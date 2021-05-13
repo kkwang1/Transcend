6903c252eec1cc4b23e097868d9f4eae3df98ab8
Kely Wang <kkwang@andrew.cmu.edu>
2021-05-13 14:10:35 -0400

    changed casing to Sidebar/Sidebar.js

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,96 @@
+import { React, useState } from "react";
+import {
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  SidebarContent,
+} from "react-pro-sidebar";
+import "./style.css";
+import Logo from "../../images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import "../../App.scss";
+
+export default function Sidebar({setOpenSidebar}) {
+  return (
+    <ProSidebar>
+      <div>
+        <img src={Logo} width="50%"></img>
+        <div style={{ float: "right", padding: "10px" }}>
+          <FontAwesomeIcon
+            icon={faTimes}
+            style={{ fontSize: "1.5em", verticalAlign: "middle" }}
+            color="#DEDEDE"
+            onClick={() => setOpenSidebar(false)}
+          />
+        </div>
+      </div>
+      <SidebarContent>
+        <Menu iconShape="circle">
+          <div className="header">Services</div>
+          <SubMenu title="Transitional Planning">
+            <MenuItem path="/education">
+              Education
+              <Link to="/transitionalplanning/education" />
+            </MenuItem>
+            <MenuItem>
+              Employment
+              <Link to="/transitionalplanning/employment" />
+            </MenuItem>
+            <MenuItem>
+              Independent living
+              <Link to="/transitionalplanning/independentliving" />
+            </MenuItem>
+            <MenuItem>
+              Integrated communities
+              <Link to="/transitionalplanning/integratedcommunities" />
+            </MenuItem>
+            <MenuItem>
+              Know your rights
+              <Link to="/transitionalplanning/rights" />v
+            </MenuItem>
+          </SubMenu>
+          <SubMenu title={"Community"}>
+            <MenuItem>
+              Virtual communities
+              <Link to="/community/virtual" />
+            </MenuItem>
+            <MenuItem>
+              Your community
+              <Link to="/community/yourcommunity" />
+            </MenuItem>
+            <MenuItem>
+              Mentorship
+              <Link to="/community/mentorship" />
+            </MenuItem>
+            <MenuItem>
+              Start your own community
+              <Link to="/community/startyourown" />
+            </MenuItem>
+          </SubMenu>
+          <MenuItem>
+            Day Program
+            <Link to="/dayprogram" />
+          </MenuItem>
+        </Menu>
+        <div className="header">Profile</div>
+        <Menu>
+          <MenuItem>
+            View profile
+            <Link to="/profile/view" />
+          </MenuItem>
+          <MenuItem>
+            Saved items
+            <Link to="/profile/saved" />
+          </MenuItem>
+          <MenuItem>
+            Reccomendations
+            <Link to="/profile/reccomendations" />
+          </MenuItem>
+        </Menu>
+      </SidebarContent>
+    </ProSidebar>
+  );
+}
