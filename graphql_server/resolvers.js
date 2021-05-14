de079506b6cae17ebab54305e5b4a13beed3f1e2
Kely Wang <kkwang@andrew.cmu.edu>
2021-05-14 18:28:09 -0400

    wrote resolvers to query a test spreadsheet successfully

--- graphql_server/resolvers.js
+++ graphql_server/resolvers.js
@@ -1,5 +1,14 @@
+const db = require('./db');
 const Query =
 {
-  greeting: () => "Hello World!"
+  searchTerm: (root, args, context, info) => {
+    const filterFn = (item => (item.name).includes(args.term));
+    return db.test.list().filter(filterFn);
+  },
+  searchCat: (root, args, context, info) => {
+    const filterFn = (item => (item.category) === args.cat);
+    return db.test.list().filter(filterFn);
+  },
+  allResults: () => db.test.list()
 }
 module.exports = { Query }
