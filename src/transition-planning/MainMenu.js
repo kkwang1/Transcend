be994445b4a318d526c02db61802f2d96994e644
cyl9011 <yolei2363@gmail.com>
2021-06-03 00:45:44 +0800

    revise transitional planning main page, add side menu component

---
+++ src/transition-planning/MainMenu.js
@@ -0,0 +1,141 @@
+import React from "react";
+import { useHistory, useLocation } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGraduationCap,
+  faBriefcase,
+  faHome,
+  faSchool,
+  faGavel,
+} from "@fortawesome/free-solid-svg-icons";
+import "./style.css";
+
+export default function MainMenu() {
+  let history = useHistory();
+  let location = useLocation();
+  const infoCol1 = [
+    {
+      title: "EDUCATION",
+      icon: faGraduationCap,
+      subitems: [
+        {
+          subtitle: "College/University",
+          url: `${location.pathname}/education/college`,
+        },
+        {
+          subtitle: "Transitional Programs",
+          url: `${location.pathname}/education/transitionalprograms`,
+        },
+      ],
+      url: `${location.pathname}/education`,
+    },
+    {
+      title: "INDEPENDENT LIVING",
+      icon: faHome,
+      subitems: [
+        {
+          subtitle: "Living at Home",
+          url: `${location.pathname}/independentliving/livingathome`,
+        },
+        {
+          subtitle: "Assisted Living",
+          url: `${location.pathname}/independentliving/assistedliving`,
+        },
+        {
+          subtitle: "Group Homes",
+          url: `${location.pathname}/independentliving/grouphomes`,
+        },
+        {
+          subtitle: "Residential Programs",
+          url: `${location.pathname}/independentliving/residentialprograms`,
+        },
+      ],
+      url: `${location.pathname}/independentliving`,
+    },
+    {
+      title: "DAY PROGRAMS",
+      icon: faSchool,
+      url: `${location.pathname}/dayprograms`,
+    },
+  ];
+
+  const infoCol2 = [
+    {
+      title: "EMPLOYMENT",
+      icon: faBriefcase,
+      subitems: [
+        {
+          subtitle: "Vocational Training",
+          url: `${location.pathname}/employment/vocationaltraining`,
+        },
+        {
+          subtitle: "Job Postings",
+          url: `${location.pathname}/employment/jobpostings`,
+        },
+        {
+          subtitle: "Career Aptitude",
+          url: `${location.pathname}/employment/career`,
+        },
+      ],
+      url: `${location.pathname}/employment`,
+    },
+    {
+      title: "KNOW YOUR RIGHTS",
+      icon: faGavel,
+      subitems: [
+        {
+          subtitle: "Funding",
+          url: `${location.pathname}/knowyourrights/funding`,
+        },
+        {
+          subtitle: "Legal Rights",
+          url: `${location.pathname}/knowyourrights/legalrights`,
+        },
+        {
+          subtitle: "Get on the List",
+          url: `${location.pathname}/knowyourrights/getonthelist`,
+        },
+      ],
+      url: `${location.pathname}/knowyourrights`,
+    },
+  ];
+
+  const menuItems = (info) => (
+    <div>
+      {info.map((s) => (
+        <div className="t-main-menu">
+          <FontAwesomeIcon
+            icon={s.icon}
+            style={{ fontSize: "40px", marginRight: "40px" }}
+          />
+          <div style={{ marginTop: "5px" }}>
+            <a href={s.url}>
+              <h5>{s.title}</h5>
+            </a>
+            {s.subitems &&
+              s.subitems.map((t) => (
+                <a href={t.url}>
+                  <p>{t.subtitle}</p>
+                </a>
+              ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+
+  return (
+    <div
+      className="layout-bottom"
+      style={{
+        width: "50%",
+        margin: "auto",
+        display: "flex",
+        justifyContent: "space-around",
+      }}
+    >
+      {menuItems(infoCol1)}
+      {menuItems(infoCol2)}
+    </div>
+  );
+}
--- src/transition-planning/index.js
+++ src/transition-planning/index.js
@@ -3,6 +3,17 @@ import { useHistory, useLocation } from "react-router";
 import "../App.scss";
 import ButtonsGroup from "../components/Button/ButtonsGroup";
 import SearchFilter from "../components/SearchAndFilter/SearchFilter";
+import SideMenu from "../components/SideMenu/SideMenu";
+import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGraduationCap,
+  faBriefcase,
+  faHome,
+  faSchool,
+  faGavel,
+} from "@fortawesome/free-solid-svg-icons";
+import MainMenu from "./MainMenu";
 // import SelectGroup from "../components/Select/SelectGroup";
 
 /* example usage of graphql:
@@ -23,7 +34,7 @@ showGreeting() {
 }
 
 */
-export default function TransitionPlanning({match}) {
+export default function TransitionPlanning({ match }) {
   let history = useHistory();
   let location = useLocation();
 
@@ -52,38 +63,52 @@ export default function TransitionPlanning({match}) {
       color: "#203E45",
       action: () => history.push(`${location.pathname}/dayprograms`),
     },
+    {
+      title: "KNOW YOUR RIGHTS",
+      backgroundColor: "#F1F1F1",
+      color: "#203E45",
+      action: () => history.push(`${location.pathname}/knowyourrights`),
+    },
   ];
 
   const selectInfo = [
     {
       title: "Filter",
-      options: ["option1", "option2", "option3"]
-
+      options: ["option1", "option2", "option3"],
     },
     {
       title: "Filter",
-      options: ["option1", "option2", "option3"]
-
+      options: ["option1", "option2", "option3"],
     },
     {
       title: "Filter",
-      options: ["option1", "option2", "option3"]
-
-    }
-  ]
+      options: ["option1", "option2", "option3"],
+    },
+  ];
 
   return (
     <div>
-      <h1 className="layout-main-header">Transitional Planning</h1>
-      <div style={{ width: "65%", margin: "auto" }}>
+      <SideMenu />
+      <div style={{ width: "65%", margin: "50px auto" }}>
+        <h1 style={{ color: "#1b7b89", marginBottom: "25px" }}>
+          Transitional Planning
+        </h1>
+        <p>
+          Find and save resources for planning transition into adulthood.
+          Utilizing the information in your profile, our system can help you
+          create an appropriate, feasible, and individualized transition plan.
+        </p>
+      </div>
+      <div style={{ width: "50%", margin: "auto", textSize: "10px" }}>
         <SearchFilter />
       </div>
-      <h3 className="layout-header" style={{ textAlign: "center" }}>
+      <h3 className="layout-header" style={{ textAlign: "center", marginTop: "40px" }}>
         Explore the following sections:{" "}
       </h3>
-      <div className="layout-btn layout-bottom" style={{ margin: "1rem 5rem"}}>
+      <MainMenu />
+      {/* <div className="layout-btn layout-bottom" style={{ margin: "1rem 5rem" }}>
         <ButtonsGroup info={btnInfo} />
-      </div>
+      </div> */}
     </div>
   );
 }
