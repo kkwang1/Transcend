009704ab74ce6acd60e35e7a3fe5fc6f3105eaaa
Kely Wang <kkwang@andrew.cmu.edu>
2021-06-22 21:15:32 -0400

    published website to build folder, to use gh-pages

---
+++ .cache/async-requires.js
@@ -0,0 +1,13 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-alternativepathways-js": () => import("./../../../src/pages/alternativepathways.js" /* webpackChunkName: "component---src-pages-alternativepathways-js" */),
+  "component---src-pages-community-js": () => import("./../../../src/pages/community.js" /* webpackChunkName: "component---src-pages-community-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-transitionalplanning-index-js": () => import("./../../../src/pages/transitionalplanning/index.js" /* webpackChunkName: "component---src-pages-transitionalplanning-index-js" */),
+  "component---src-pages-transitionalplanning-main-menu-js": () => import("./../../../src/pages/transitionalplanning/MainMenu.js" /* webpackChunkName: "component---src-pages-transitionalplanning-main-menu-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,15 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\kelly\\localProjects\\Transcend\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\kelly\\localProjects\\Transcend\\src\\pages\\404.js")),
+  "component---src-pages-alternativepathways-js": preferDefault(require("C:\\Users\\kelly\\localProjects\\Transcend\\src\\pages\\alternativepathways.js")),
+  "component---src-pages-community-js": preferDefault(require("C:\\Users\\kelly\\localProjects\\Transcend\\src\\pages\\community.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\kelly\\localProjects\\Transcend\\src\\pages\\index.js")),
+  "component---src-pages-transitionalplanning-index-js": preferDefault(require("C:\\Users\\kelly\\localProjects\\Transcend\\src\\pages\\transitionalplanning\\index.js")),
+  "component---src-pages-transitionalplanning-main-menu-js": preferDefault(require("C:\\Users\\kelly\\localProjects\\Transcend\\src\\pages\\transitionalplanning\\MainMenu.js"))
+}
+
