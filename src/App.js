b83718e1a682f716054a2948ccad833206c5a1d0
cyl9011 <yolei2363@gmail.com>
2021-08-19 20:38:09 -0400

    revert route base name

--- src/App.js
+++ src/App.js
@@ -21,10 +21,10 @@ function App() {
         <NavBar openSidebar={open} setOpenSidebar={setOpen} />
         <Switch>
           <Route exact path="/" component={Main} />
-          <Route path={process.env.PUBLIC_URL + "/transitionalplanning"} component={TransitionPlanning} />
-          <Route path={process.env.PUBLIC_URL + "/community"} component={Community} />
-          <Route path={process.env.PUBLIC_URL + "/alternativepathways"} component={AlternativePathways} />
-          <Route path={process.env.PUBLIC_URL+ "/createaccount"} component={CreateAccount} />
+          <Route path={"/transitionalplanning"} component={TransitionPlanning} />
+          <Route path={"/community"} component={Community} />
+          <Route path={"/alternativepathways"} component={AlternativePathways} />
+          <Route path={"/createaccount"} component={CreateAccount} />
         </Switch>
       </div>
     </Router>
