7559e219151a63de881d99631d0e9458ea84e5e9
cyl9011 <yolei2363@gmail.com>
2021-12-29 23:27:47 -0500

    add community and alt pathways placeholders

--- src/pages/alternative-pathways/index.js
+++ src/pages/alternative-pathways/index.js
@@ -1,5 +1,34 @@
-import React, { useState } from 'react';
+import { Modal } from "react-bootstrap";
+import React, { useState } from "react";
+import alternativePathwaysImg from "../../images/alt-path.png";
+import { useHistory } from "react-router";
 
-export default function AlternativePathways(){
-    return <></>
+export default function AlternativePathways() {
+  let history = useHistory();
+  
+  return (
+      <Modal
+        show
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header
+          closeButton
+          onHide={() => {
+            history.goBack();
+          }}
+        >
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            src={alternativePathwaysImg}
+            style={{ width: "-webkit-fill-available" }}
+          ></img>
+        </Modal.Body>
+      </Modal>
+  );
 }
--- src/pages/community/index.js
+++ src/pages/community/index.js
@@ -1,5 +1,33 @@
-import React, { useState } from 'react';
+import { Modal } from "react-bootstrap";
+import React, { useState } from "react";
+import communityImg from "../../images/community.png";
+import { useHistory } from "react-router";
+export default function Community() {
+  let history = useHistory();
 
-export default function Community(){
-    return <></>
+  return (
+    <Modal
+      show
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header
+        closeButton
+        onHide={() => {
+          history.goBack();
+        }}
+      >
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <img
+          src={communityImg}
+          style={{ width: "-webkit-fill-available" }}
+        ></img>
+      </Modal.Body>
+    </Modal>
+  );
 }
